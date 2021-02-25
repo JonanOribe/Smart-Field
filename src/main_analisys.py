b2e1c197bb28cd6b6dcc03836e4c37d211c1b089
jonanOribe <jonan.oribe@gmail.com>
2021-02-25 13:29:33 +0100

    Refractor

--- src/main_analisys.py
+++ src/main_analisys.py
@@ -1,10 +1,13 @@
 import os
 import pandas as pd
-from main_utils import csv_from_excel, get_files_with_data
+from main_utils import dendometer_and_battery_cleaner, get_files_with_data
+
 PROYECT_PATH=os.getcwd()
+
 xlsx_arr=get_files_with_data()
 
 for data_block in xlsx_arr:
     file_path='{}{}{}'.format(PROYECT_PATH,'/data/',data_block)
     df=pd.read_excel(file_path)
+    df=dendometer_and_battery_cleaner(df)
     print(df)
--- src/main_utils.py
+++ src/main_utils.py
@@ -1,7 +1,6 @@
 from os import listdir
 import os
 from os.path import isfile, join
-import xlrd
 import csv
 from configparser import ConfigParser
 
@@ -12,17 +11,8 @@ config.read('config.cfg')
 data_path = config['DEFAULT']['data_path']
 
 def get_files_with_data():
-    return [f for f in listdir(data_path) if '.xlsx' in f]
+    return [f for f in listdir(data_path) if ('.xlsx' in f and not 'lock.' in f)]
 
-def csv_from_excel(xlsx_file):
-    wb = xlrd.open_workbook(xlsx_file)
-    sh = wb.sheet_by_name('Sheet1')
-    your_csv_file = open('your_csv_file.csv', 'w')
-    wr = csv.writer(your_csv_file, quoting=csv.QUOTE_ALL)
-
-    for rownum in range(sh.nrows):
-        wr.writerow(sh.row_values(rownum))
-
-    your_csv_file.close()
-
-# runs the csv_from_excel function:
+def dendometer_and_battery_cleaner(df):
+    df.drop(['Unnamed: 0', 'Batería de la estacion'], axis = 1, inplace = True)
+    return df[df['Dendrómetro'].notna()]
