fdd883ee29f9d3a312218a79280b66b151dd2745
jonanOribe <jonan.oribe@gmail.com>
2021-02-28 12:31:04 +0100

    Reporting results

--- src/main_analisys.py
+++ src/main_analisys.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
-from main_utils import dendometer_and_battery_cleaner, get_files_with_data
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from termcolor import colored 
+
+from main_utils import dendometer_and_battery_cleaner, generate_decision_tree, generate_reports, get_files_with_data
 
 PROYECT_PATH=os.getcwd()
 
@@ -12,4 +15,11 @@ for data_block in xlsx_arr:
     df=dendometer_and_battery_cleaner(df)
     df_columns=df.columns
     df[df_columns] = df[df_columns].apply(pd.to_numeric)
-    print(df.corr())
+
+    clf,X_train,X_test,y_train,y_test=generate_decision_tree(df,df_columns)
+
+    #Predict the response for test dataset
+    y_pred = clf.predict(X_test)
+    # Model Accuracy, how often is the classifier correct?
+    print(colored("Accuracy:",'green'),metrics.accuracy_score(y_test.astype('int'), y_pred.astype('int')))
+    generate_reports(X_train,X_test)
--- src/main_utils.py
+++ src/main_utils.py
@@ -3,6 +3,9 @@ import os
 from os.path import isfile, join
 import csv
 from configparser import ConfigParser
+import sweetviz
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn.model_selection import train_test_split # Import train_test_split function
 
 PROYECT_PATH=os.getcwd()
 config = ConfigParser()
@@ -16,3 +19,20 @@ def get_files_with_data():
 def dendometer_and_battery_cleaner(df):
     df.drop(['FECHA', 'BAT'], axis = 1, inplace = True)
     return df[df['TD'].notna()]
+
+def generate_decision_tree(df,df_columns):
+    #split dataset in features and target variable
+    feature_cols = [a for a in df_columns if a not in ['SOILT']]
+    X = df[feature_cols] # Features
+    y = df['SOILT'] # Target variable
+    # Split dataset into training set and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1) # 70% training and 30% test
+    # Create Decision Tree classifer object
+    clf = DecisionTreeClassifier()
+
+    # Train Decision Tree Classifer
+    return clf.fit(X_train,y_train.astype('int')),X_train,X_test,y_train,y_test
+
+def generate_reports(X_train,X_test):
+    report = sweetviz.compare([X_train, "Train"], [X_test, "Test"], "TCB")
+    report.show_html("informe_datos.html")
