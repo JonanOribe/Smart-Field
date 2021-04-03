5eec99f55a73f17edfaf01b7393f436a834de203
jonanOribe <jonan.oribe@gmail.com>
2021-04-03 12:06:25 +0200

    Update

--- src/API_conection.py
+++ src/API_conection.py
@@ -1,11 +1,16 @@
 
 import requests
 import json
+from configparser import ConfigParser
+
+config = ConfigParser()
+config.read('config.cfg')
+aemet_api=config['DEFAULT']['aemet_api']
 
 municipio="09059"
-url = "https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/"+municipio
+url = '{}{}'.format("https://opendata.aemet.es/opendata/api/prediccion/especifica/municipio/horaria/",municipio)
 
-querystring = {"api_key":"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb25hbi5vcmliZUBnbWFpbC5jb20iLCJqdGkiOiIyYTM0ZDFlOS02YzU3LTRjNjQtYWM1Ni0yZWJiNWY0ZmIzMjEiLCJpc3MiOiJBRU1FVCIsImlhdCI6MTYxNzQ0MTc5NSwidXNlcklkIjoiMmEzNGQxZTktNmM1Ny00YzY0LWFjNTYtMmViYjVmNGZiMzIxIiwicm9sZSI6IiJ9.I2epfuKBmHXggrPa-eKFxvgjpUK8IX42hiZ7nCKqNtc"}
+querystring = {"api_key":aemet_api}
 
 headers = {
     'cache-control': "no-cache"
