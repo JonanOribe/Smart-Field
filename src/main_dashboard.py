dd92df494586142ad0e158b481a6148a8480cada
jonanOribe <jonan.oribe@gmail.com>
2021-04-18 11:10:58 +0200

    Update main_dashboard.py

--- src/main_dashboard.py
+++ src/main_dashboard.py
@@ -15,7 +15,7 @@ st.title('ALTAR data')
 DATE_COLUMN = 'FECHA'
 DATA_URL = get_files_with_data()
 
-col1, col2, col3= st.beta_columns((2,2,2))
+col1, col2, col3= st.beta_columns((2,1,1))
 
 @st.cache
 def load_data(nrows):
@@ -27,24 +27,22 @@ data_load_state = st.text('Loading data...')
 #data = load_data(100)
 data = pd.read_excel(data_path+'/'+DATA_URL[0], skiprows=1)
 data = data[data['TD'].notna()]
-data.drop(['TD','BAT','PAR'], axis=1, inplace=True)
+data.drop(['TD','BAT','PAR','PLV','WV'], axis=1, inplace=True)
+data=data.head(500)
 data_load_state.text("Done! (using st.cache)")
 
 sensor_errors=pd.read_csv("sensor_errors.csv").set_index('Error_Type')
 
-if col1.checkbox('Show raw data'):
-    col1.subheader('Raw data')
-    col1.write(data)
+#if col1.checkbox('Show raw data'):
+col1.subheader('Raw data')
+col1.write(data)
 
-if col2.checkbox('Show errors data'):
-    grouped_data=sensor_errors.groupby(['Error_Type']).size()
-    col2.subheader('Sensor´s errors data')
-    col2.write(sensor_errors)
-    col3.text("")
-    col3.text("")
-    col3.text("")
-    col3.subheader('Grouped by error type')
-    col3.bar_chart(grouped_data)
+#if col2.checkbox('Show errors data'):
+grouped_data=sensor_errors.groupby(['Error_Type']).size()
+col2.subheader('Sensor´s errors data')
+col2.write(sensor_errors)
+col3.subheader('Grouped by error type')
+col3.bar_chart(grouped_data)
 
 humb_to_filter = st.slider('TCB', float(data['TCB'].min()), float(data['TCB'].max()),(float(data['TCB'].min()), float(data['TCB'].max())))
 
